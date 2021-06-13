c20a04d81549abc50bbef93dbcf6fa4cc3109c2f
Dhruvish09 <pateldhruvish612000@gmail.com>
2021-06-13 18:47:46 +0530

    update

--- pro/urls.py
+++ pro/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+admin.site.site_header = "SBP Admin"
+admin.site.site_title = "SBP"
+admin.site.index_title = "SBP Admin Portal"
+
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
